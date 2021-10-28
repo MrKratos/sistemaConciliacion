a820af5d0dd2cc36eca7f6341009cdb8fc246b70
Nicolasjdpc <nicolasjdpc@gmail.com>
2021-10-27 22:41:01 -0500

    primer commit
    primer commit

---
+++ Controllers/ejecutarProcController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppCuadre.Datos;
+using AppCuadre.Models;
+
+namespace AppCuadre.Controllers
+{
+    public class ejecutarProcController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View("ejecutarProcedimiento");
+        }
+    }
+}
