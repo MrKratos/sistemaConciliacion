b6ae599046d6992a3413fad1e2fe7e26caca35e8
Nicolasjdpc <nicolasjdpc@gmail.com>
2021-10-28 23:03:37 -0500

    creacion de tablas por metodo
    crea tablas, se quedo en media parte lo de insertar en la tabla creada

---
+++ Controllers/subirDatos.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AppCuadre.Controllers
+{
+    public class subirDatos : Controller
+    {
+        public IActionResult SubirDatos()
+        {
+            return View();
+        }
+        public IActionResult archivos()
+        {
+            return RedirectToAction("SubirDatos");
+        }
+
+        public void ExtraccionDatos(String procedimiento, DataTable tabla) {
+            String[] campos = camposProcedimiento("");
+            String nombreTablas = nombreTabla("");
+            String query = "insert into " + nombreTablas + " (";
+
+            for (int j = 0; j < campos.Length; j++) {
+                query = query +" " +campos[j];
+                if (j == campos.Length - 1){
+                    query = query + ") values (";
+                }
+                else {
+                    query = query + " ,";
+                }
+            }
+
+
+            for (int i = 0; i < tabla.Rows.Count; i++) {
+                for (int k=0; k< campos.Length; k++) {
+                    
+                }
+            }
+        }
+
+        public String[] camposProcedimiento(String procedimiento) {
+            /*seleccionar campos documento*/
+            String[] campos = { "valor", "texto" };
+            return campos;
+        }
+
+        public String nombreTabla(String procedimiento) {
+            return "conciliaciones";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,14 +10,93 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace AppCuadre
 {
+    public class Campo
+    {
+        public string nombre { get; set; }
+        public int tipoDato { get; set; }
+    }
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            List<Campo> lista = new List<Campo>() {
+             new Campo(){nombre="valor", tipoDato=1 },
+             new Campo(){nombre="texto", tipoDato=1 },
+            };
+            CrearTablaDatosEmpresa("conciliacion", lista);
+        }
+
+
+       
+
+
+        protected SqlConnection ObtenerConexion()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "NICOLAS\\SQLEXPRESS";
+            builder.InitialCatalog = "conciliaciones";
+            builder.UserID = "NicolasMinds";
+            builder.Password = "093933580";
+            builder.ApplicationName = "MyApp";
+
+            return new SqlConnection(builder.ConnectionString);
+        }
+
+        public Boolean CrearTablaDatosEmpresa(string nombre, List<Campo> datos )
+        {
+            Boolean estado = false;
+            string cadenaCreacionTabla = " IF NOT EXISTS (select * from sysobjects where name='"+nombre+"')  create table " + nombre + "( Id" + nombre + " int identity(1,1) primary key,";
+            string campos = cadenaCreacionTabla;
+
+            for (int i = 0; i < datos.Count; i++) {
+
+                if (datos[i].tipoDato == 1)
+                {
+                    campos = campos + datos[i].nombre + " varchar(200) ";
+                }
+                else if (datos[i].tipoDato == 2)
+                {
+                    campos = campos + datos[i].nombre + " money";
+                }
+                else
+                {
+                    campos = campos + datos[i].nombre + "int";
+                }
+                if (i==datos.Count-1) {
+                    campos = campos + " ) select 0; ELSE select 1";
+                } 
+                else{
+                    campos = campos + " ,"; 
+                }
+                        
+
+               
+            }
+
+            using (var conexion = ObtenerConexion())
+            {
+                Int32 newProdID = 0;
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = campos;
+                    comando.CommandType = CommandType.Text;
+                    /*newProdID=(Int32) comando.ExecuteScalar();
+                    if (newProdID == 1) {
+                        estado = false;
+                    }else {
+                        estado = true;
+                    } */
+                }
+            }
+            return estado;
         }
 
         public IConfiguration Configuration { get; }
